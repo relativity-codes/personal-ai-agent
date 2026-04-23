8a3eb23e5ec748fb766a5104c2ae94845a514c02
relativity-codes <ukweheverest@gmail.com>
2026-04-23 13:47:55 +0100

    fix imports issues and pydantic reponse issues

--- backend/app/api/routers/v1/session_router.py
+++ backend/app/api/routers/v1/session_router.py
@@ -2,17 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_db
 from app.db.repositories.session_repository import SessionRepository
-from app.db.models.session import Session
+from app.api.schemas import SessionRead
 from uuid import UUID
 
 router = APIRouter()
 
-@router.post("/", response_model=Session)
+@router.post("/", response_model=SessionRead)
 async def create_session(user_id: UUID, db: AsyncSession = Depends(get_db)):
     repo = SessionRepository
     return await repo.create(db, user_id=user_id)
 
-@router.get("/{session_id}", response_model=Session)
+@router.get("/{session_id}", response_model=SessionRead)
 async def read_session(session_id: UUID, db: AsyncSession = Depends(get_db)):
     repo = SessionRepository
     db_session = await repo.get_by_id(db, session_id)
@@ -20,7 +20,7 @@ async def read_session(session_id: UUID, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Session not found")
     return db_session
 
-@router.get("/user/{user_id}", response_model=list[Session])
+@router.get("/user/{user_id}", response_model=list[SessionRead])
 async def read_sessions_by_user(user_id: UUID, skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
     repo = SessionRepository
     return await repo.get_all_by_user_id(db, user_id, skip, limit)
